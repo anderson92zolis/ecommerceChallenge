fe4f92798ed1dbe93993413d8cc9f078798a0c8a
anderson92zolis <azolis92@hotmail.es>
2023-10-19 19:12:26 +0200

    creating a application.yml and application

---
+++ productsmicroservices/src/main/java/org/ecommerce/products/productsApp.java
@@ -0,0 +1,13 @@
+package org.ecommerce.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@SpringBootApplication
+public class productsApp {
+        public static void main(String[] args) {
+            SpringApplication.run(productsApp.class, args);
+        }
+}
