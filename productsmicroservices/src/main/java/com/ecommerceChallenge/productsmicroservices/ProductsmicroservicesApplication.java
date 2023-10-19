d33ccf45e04acf0a1d3e5b2e4a1e022f2396995f
anderson92zolis <azolis92@hotmail.es>
2023-10-19 18:48:27 +0200

    creating the commit the products layer with Initialzr

---
+++ productsmicroservices/src/main/java/com/ecommerceChallenge/productsmicroservices/ProductsmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceChallenge.productsmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsmicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsmicroservicesApplication.class, args);
+	}
+
+}
---
+++ productsmicroservices/src/test/java/com/ecommerceChallenge/productsmicroservices/ProductsmicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceChallenge.productsmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsmicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
