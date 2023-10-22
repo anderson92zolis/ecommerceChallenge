4e30f09f1e1fd536d93df80cbeb44fef6f9227c0
anderson92zolis <azolis92@hotmail.es>
2023-10-22 17:03:59 +0200

    notihing to commit

--- productsmicroservices/src/main/java/org/ecommerce/products/entity/Product.java
+++ productsmicroservices/src/main/java/org/ecommerce/products/entity/Product.java
@@ -2,14 +2,12 @@ package org.ecommerce.products.entity;
 
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="products")
+@Table(name="product")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
