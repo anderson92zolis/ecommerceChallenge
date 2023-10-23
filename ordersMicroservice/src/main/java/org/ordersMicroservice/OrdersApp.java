0c84dc7b0e74fa4a857f6f237dbcd62a53bf2bd4
anderson92zolis <azolis92@hotmail.es>
2023-10-23 17:37:25 +0200

    adding the application.yml and the OrdersApp

---
+++ ordersMicroservice/src/main/java/org/ordersMicroservice/OrdersApp.java
@@ -0,0 +1,14 @@
+package org.ordersMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@SpringBootApplication
+public class OrdersApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersApp.class,args);
+
+    }
+}
