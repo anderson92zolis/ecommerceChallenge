7c81ba54c6cb54704fc2194a14bc377f4543f07e
anderson92zolis <azolis92@hotmail.es>
2023-10-31 09:55:02 +0100

    testing for Enum

---
+++ productsmicroservices/src/test/java/org/ecommerce/products/dto/ProductResponseTest.java
@@ -0,0 +1,48 @@
+package org.ecommerce.products.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductResponseTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void builder() {
+    }
+
+    @Test
+    void testGetName() {
+    }
+
+    @Test
+    void testGetDescription() {
+    }
+
+    @Test
+    void testGetCategory() {
+    }
+
+    @Test
+    void testGetPrice() {
+    }
+
+    @Test
+    void testBuilder() {
+    }
+}
---
+++ productsmicroservices/src/test/java/org/ecommerce/products/dto/ProductsRequestTest.java
@@ -0,0 +1,36 @@
+package org.ecommerce.products.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductsRequestTest {
+
+    @Test
+    void getSku() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void getManufacturer() {
+    }
+
+    @Test
+    void getSupplier() {
+    }
+}
