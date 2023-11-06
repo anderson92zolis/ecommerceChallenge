c3746f28d53da3c9d758e0f4b9ec63a9c6421f5f
anderson92zolis <azolis92@hotmail.es>
2023-11-07 00:17:02 +0100

    adding eureka server but the clients do not work

--- ordersMicroservice/src/main/java/org/ordersMicroservice/OrdersApp.java
+++ ordersMicroservice/src/main/java/org/ordersMicroservice/OrdersApp.java
@@ -2,10 +2,12 @@ package org.ordersMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 @SpringBootApplication
+@EnableEurekaClient
 public class OrdersApp {
     public static void main(String[] args) {
         SpringApplication.run(OrdersApp.class,args);
