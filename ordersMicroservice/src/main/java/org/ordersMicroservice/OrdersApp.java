9de76d442550b00655d778444df628fe85cbfa7c
anderson92zolis <azolis92@hotmail.es>
2023-11-07 10:12:43 +0100

    adding the eureka spring cloud

--- ordersMicroservice/src/main/java/org/ordersMicroservice/OrdersApp.java
+++ ordersMicroservice/src/main/java/org/ordersMicroservice/OrdersApp.java
@@ -2,12 +2,12 @@ package org.ordersMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class OrdersApp {
     public static void main(String[] args) {
         SpringApplication.run(OrdersApp.class,args);
--- productsmicroservices/src/main/java/org/ecommerce/products/ProductsApp.java
+++ productsmicroservices/src/main/java/org/ecommerce/products/ProductsApp.java
@@ -2,12 +2,13 @@ package org.ecommerce.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ProductsApp {
         public static void main(String[] args) {
             SpringApplication.run(ProductsApp.class, args);
