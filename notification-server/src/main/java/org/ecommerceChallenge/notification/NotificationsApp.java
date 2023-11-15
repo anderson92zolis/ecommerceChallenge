e07fdf0abcfcc20e4c39c2999b39e1295be74b4b
anderson92zolis <azolis92@hotmail.es>
2023-11-15 17:25:37 +0100

    adding a CommandLineRunner to our topic

--- notification-server/src/main/java/org/ecommerceChallenge/notification/NotificationsApp.java
+++ notification-server/src/main/java/org/ecommerceChallenge/notification/NotificationsApp.java
@@ -1,7 +1,10 @@
 package org.ecommerceChallenge.notification;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class NotificationsApp {
@@ -10,4 +13,12 @@ public class NotificationsApp {
         SpringApplication.run(NotificationsApp.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+        return args -> {
+            kafkaTemplate.send("notificationEcommerce","hello Kafka");
+        };
+    }
 }
+
+
