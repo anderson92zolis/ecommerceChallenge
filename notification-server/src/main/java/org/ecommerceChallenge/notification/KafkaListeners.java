02057577c58d8fa2ad374293a85e3f61cc1b64b8
anderson92zolis <azolis92@hotmail.es>
2023-11-15 18:23:24 +0100

    adding the Kafka Listners and endpoint postman

--- notification-server/src/main/java/org/ecommerceChallenge/notification/KafkaListeners.java
+++ notification-server/src/main/java/org/ecommerceChallenge/notification/KafkaListeners.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaListeners {
+
+
     @KafkaListener(
             topics="notificationEcommerce",
             groupId= "groudId"
