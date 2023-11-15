50976e06d731d86f2fe211abec39d7458650b86a
anderson92zolis <azolis92@hotmail.es>
2023-11-15 17:01:35 +0100

    adding kafka, notification service, dependecies, KaflaProducerConfig, KafkaTopicConfig

---
+++ notification-server/src/main/java/org/ecommerceChallenge/notification/NotificationsApp.java
@@ -0,0 +1,13 @@
+package org.ecommerceChallenge.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationsApp.class, args);
+    }
+
+}
