c6d9cc05fd6b7a7df96fa82ab9daf7026a72efd4
Josep SS <josepsalgado@hotmail.com>
2023-12-06 20:48:19 +0100

    Commit 9

--- productsmicroservices/src/main/java/org/ecommerce/products/service/Methods.java
+++ productsmicroservices/src/main/java/org/ecommerce/products/service/Methods.java
@@ -18,7 +18,14 @@ public class Methods {
         return result;
     }
 
-    public static int subtract (int a, int b) {
+    public static int suma4(int a, int b, int c, int d) {
+        int result;
+        result = a + b + c + d;
+
+        return result;
+    }
+
+    public static int subtract(int a, int b) {
         int result;
         result = a - b;
 
