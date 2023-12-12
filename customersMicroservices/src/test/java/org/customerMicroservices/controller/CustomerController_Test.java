0115b8afcadb78c63886f622b80dd7797afda975
Josep SS <josepsalgado@hotmail.com>
2023-12-12 11:48:21 +0100

    Endpoint Test

---
+++ customersMicroservices/src/test/java/org/customerMicroservices/controller/CustomerController_Test.java
@@ -0,0 +1,33 @@
+package org.customerMicroservices.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebFluxTest(CustomerController.class)
+class CustomerController_Test {
+    //region VARIABLES
+    @Autowired
+    private WebTestClient webTestClient;
+
+    //endregion VARIABLES
+
+
+    //region TESTS
+    @Test
+    void test1() {
+        webTestClient.get()
+                .uri("/api/v1/customers/test")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .isEqualTo("Hello from CUSTOMER!!!");
+    }
+
+    //endregion TESTS
+
+}
+
