a385d29aab748bb9a0c2646038893ee20151dc5d
Josep SS <josepsalgado@hotmail.com>
2024-01-10 00:11:58 +0100

    End of day

---
+++ customersMicroservices/src/main/java/org/customerMicroservices/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.customerMicroservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+//TODO
+    //    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpIn) throws Exception{
+//
+//    }
+}
